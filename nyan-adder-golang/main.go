6066948eb0a7bd0e27b3c38de99df76c934e66f1
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-08-15 14:28:04 +0000

    fiuh finally

--- nyan-adder-golang/main.go
+++ nyan-adder-golang/main.go
@@ -5,43 +5,24 @@ import (
     "syscall/js"
 )
 
-func FuncName(M int, durations []int) int {
-    fmt.Println("[GO] Log file.go", M, durations)
-    return 10
+
+func nyanify(word string) string {
+    result := word + "nyan"
+    //fmt.Println(result)
+    return result
 }
 
 func main() {
     fmt.Println("Hello, WebAssembly!")
-
     // Mount the function on the JavaScript global object.
-    js.Global().Set("FuncName", js.FuncOf(func(this js.Value, args []js.Value) any {
-        if len(args) != 2 {
-            fmt.Println("invalid number of args")
-            return nil
+    js.Global().Set("nyanify", js.FuncOf(func(this js.Value, args []js.Value) any {
+        if len(args) != 1 {
+            //fmt.Println("invalid number of args")
+            return "Invalid no of arguments passed"
         }
-        if args[0].Type() != js.TypeNumber {
-            fmt.Println("the first argument should be a number")
-            return nil
-        }
-
-        arg := args[1]
-        if arg.Type() != js.TypeObject {
-            fmt.Println("the second argument should be an array")
-            return nil
-        }
-
-        durations := make([]int, arg.Length())
-        for i := 0; i < len(durations); i++ {
-            item := arg.Index(i)
-            if item.Type() != js.TypeNumber {
-                fmt.Printf("the item at index %d should be a number\n", i)
-                return nil
-            }
-            durations[i] = item.Int()
-        }
-
-        // Call the actual func.
-        return FuncName(args[0].Int(), durations)
+        input := args[0].String()
+        fmt.Printf("input %s\n", input)
+        return nyanify(input)
     }))
 
     // Prevent the program from exiting.
